470babd9823d322a5e9f5fe8d725d3cd2bf8b356
unaihuete93 <64772417+unaihuete93@users.noreply.github.com>
2021-10-04 13:25:05 +0000

    not found controller

--- apis/poi/web/Controllers/POIController.cs
+++ apis/poi/web/Controllers/POIController.cs
@@ -35,6 +35,7 @@ namespace poi.Controllers
             //     return NotFound();
             // }
             // return Ok(item);
+            return NotFound();
         }
 
         [HttpGet("trip/{tripID}", Name = "GetPOIsByTripId")]
